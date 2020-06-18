1017ed9f9ef3f7969ab53b0a107a58f67b26f760
freev53 <57835605+freev53@users.noreply.github.com>
2020-06-18 17:02:08 +0900

    김범수 Branch 1

--- app/src/main/java/com/example/yourschedule/LoadingActivity.java
+++ app/src/main/java/com/example/yourschedule/LoadingActivity.java
@@ -12,7 +12,8 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-      
+
+        //김범수 Branch1
         Handler handler = new Handler(){
 
             @Override
