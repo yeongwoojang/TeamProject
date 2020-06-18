2cabbeb8b1824bc9843715ca312bb88534b4a125
user <https://github.com/yeongwoojang/ourGroup.git>
2020-06-18 17:01:37 +0900

    장영우브랜치1

--- app/src/main/java/com/example/yourschedule/LoadingActivity.java
+++ app/src/main/java/com/example/yourschedule/LoadingActivity.java
@@ -12,7 +12,7 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-      
+        //장영우브랜치1
         Handler handler = new Handler(){
 
             @Override
