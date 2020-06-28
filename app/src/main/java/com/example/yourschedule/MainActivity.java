36da8dcc57cdffeb9d4e73edf2cb7b9ebc4de229
freev53 <57835605+freev53@users.noreply.github.com>
2020-06-28 19:52:47 +0900

    firebase_kakao 토큰 넘기기

--- app/src/main/java/com/example/yourschedule/MainActivity.java
+++ app/src/main/java/com/example/yourschedule/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.yourschedule;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.yourschedule.FRAGMENT.MyList;
 import com.example.yourschedule.FRAGMENT.ScheduleList;
@@ -269,7 +268,6 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             } else {
                                 Log.d("KAKAO_API", kakaoAccount + "");
-
                             }
                         }
                     });
