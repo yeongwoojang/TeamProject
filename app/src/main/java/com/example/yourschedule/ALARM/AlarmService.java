c626bb6b7758548ccdc8cd277d34e07b8293c60d
user <https://github.com/yeongwoojang/ourGroup.git>
2020-08-19 16:28:15 +0900

    Merge remote-tracking branch 'remotes/origin/yeongwoojang'
    # Conflicts:
    #	app/src/main/java/com/example/yourschedule/DECORATOR/TextSizeDecorator.java
    #	app/src/main/java/com/example/yourschedule/FRAGMENT/Calandar.java
    #	app/src/main/res/layout/fragment_calandar.xml

--- app/src/main/java/com/example/yourschedule/ALARM/AlarmService.java
+++ app/src/main/java/com/example/yourschedule/ALARM/AlarmService.java
@@ -8,10 +8,12 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,16 +25,60 @@ import com.example.yourschedule.R;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class AlarmService extends IntentService {
-
-
-    public AlarmService(String name) {
-        super(name);
-    }
-
+public class AlarmService extends Service {
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        String requestCode = rootIntent.getStringExtra("requestCode");
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, Integer.parseInt(requestCode),
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 쓰면 Oreo 이상에서 시스템 UI 에러난다..
+
+            String channelId = "default";
+            String channelName = "일정 알람 채널";
+            String description = "API 26버전 이상을 위한 것입니다.";
+
+            //NotificationManager.IMPORTANCE_HIGH :  소리와 알림메시지를 같이 보여줌
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(description);
+
+            if (notificationManager != null) {
+                // 노티피케이션 채널을 시스템에 등록
+                notificationManager.createNotificationChannel(channel);
+            }
+        }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
+
+        builder.setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setTicker("{Time to watch some cool stuff!}")
+                .setContentTitle("YourList")
+                .setContentText("오늘 일정을 확인하세요!")
+                .setContentInfo("INFO")
+                .setContentIntent(pendingIntent);
+        if (notificationManager != null) {
+            // 노티피케이션 동작시킴
+            notificationManager.notify(1234, builder.build());
+        }
+    }
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
         String requestCode = intent.getStringExtra("requestCode");
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -72,166 +118,14 @@ public class AlarmService extends IntentService {
                 .setContentIntent(pendingIntent);
         if (notificationManager != null) {
             // 노티피케이션 동작시킴
-//            SimpleDateFormat fm = new SimpleDateFormat("yyyy.MM.dd");
-//            Calendar calendar = Calendar.getInstance();
-//            String CompareDate = fm.format(calendar.getTime());
-//            String year = CompareDate.substring(0,10).substring(0,4);
-//            String month = CompareDate.substring(0,10).substring(5,7);
-//            String day = CompareDate.substring(8,10);
-//            SharedPreferences sharedPreferences= getSharedPreferences("daily alarm", MODE_PRIVATE);
-//            long time = sharedPreferences.getLong(year+"."+month+"."+day, calendar.getTimeInMillis());
-//            if(calendar.getTimeInMillis()==time){
-//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-////                    startForeground(1, builder.build());
-//                }
-//            Log.d("noti","Beforenoti");
             notificationManager.notify(1234, builder.build());
-////                thread.stopForever();
-//            }
         }
+        return START_STICKY;
     }
 
+    @Nullable
     @Override
-    public void onTaskRemoved(Intent rootIntent) {
-        stopSelf();
-    }
-
-//    @Override
-//    public void onTaskRemoved(Intent rootIntent) {
-//        Log.d("service","onTaskRemoved()");
-//        SimpleDateFormat fm = new SimpleDateFormat("yyyy.MM.dd");
-//        Calendar calendar = Calendar.getInstance();
-//        String CompareDate = fm.format(calendar.getTime());
-//        String year = CompareDate.substring(0,10).substring(0,4);
-//        String month = CompareDate.substring(0,10).substring(5,7);
-//        String day = CompareDate.substring(8,10);
-//        Intent alarmServiceIntent = new Intent(getApplicationContext(),AlarmReceiver.class);
-//        SharedPreferences sharedPreferences = getSharedPreferences("daily alarm", MODE_PRIVATE);
-//        long millis = sharedPreferences.getLong(CompareDate, Calendar.getInstance().getTimeInMillis());
-//
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), Integer.parseInt(year+month+day), alarmServiceIntent, 0);
-//
-//        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-//
-//        if (alarmManager != null) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-////                    alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(time,pendingIntent),pendingIntent);
-//                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, millis, pendingIntent);
-//            }else{
-//                alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), pendingIntent);
-//            }
-//        }
-//        stopSelf();
-////        super.onTaskRemoved(rootIntent);
-//    }
-
-//    private Handler mHandler = new Handler(){
-//        public void handleMessage(Message msg){
-//            Log.d("service","handleMessage()");
-//            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//
-//            Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-//            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-//
-//            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-//                    notificationIntent, 0);
-//
-//            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "default");
-//
-//
-//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-//                builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
-//
-//                String channelId = "default";
-//                String channelName = "일정 알람 채널";
-//                String description = "API 26버전 이상을 위한 것입니다.";
-//
-//                //NotificationManager.IMPORTANCE_HIGH :  소리와 알림메시지를 같이 보여줌
-//                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-//                channel.setDescription(description);
-//
-//                if (notificationManager != null) {
-//                    // 노티피케이션 채널을 시스템에 등록
-//                    notificationManager.createNotificationChannel(channel);
-//                }
-//            }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
-//
-//            builder.setAutoCancel(true)
-//                    .setDefaults(NotificationCompat.DEFAULT_ALL)
-//                    .setWhen(System.currentTimeMillis())
-//                    .setTicker("{Time to watch some cool stuff!}")
-//                    .setContentTitle("YourList")
-//                    .setContentText("오늘 일정을 확인하세요!")
-//                    .setContentInfo("INFO")
-//                    .setContentIntent(pendingIntent);
-//            if (notificationManager != null) {
-//                // 노티피케이션 동작시킴
-//
-//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-//                    startForeground(1, builder.build());
-//                }
-//                    notificationManager.notify(1234, builder.build());
-//            }
-//        }
-//    };
-    public class ServiceHandler extends Handler {
-
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            Log.d("service","handleMessage()");
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-            Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-                    notificationIntent, 0);
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "default");
-
-
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
-
-                String channelId = "default";
-                String channelName = "일정 알람 채널";
-                String description = "API 26버전 이상을 위한 것입니다.";
-
-                //NotificationManager.IMPORTANCE_HIGH :  소리와 알림메시지를 같이 보여줌
-                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-                channel.setDescription(description);
-
-                if (notificationManager != null) {
-                    // 노티피케이션 채널을 시스템에 등록
-                    notificationManager.createNotificationChannel(channel);
-                }
-            }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
-
-            builder.setAutoCancel(true)
-                    .setDefaults(NotificationCompat.DEFAULT_ALL)
-                    .setWhen(System.currentTimeMillis())
-                    .setTicker("{Time to watch some cool stuff!}")
-                    .setContentTitle("YourList")
-                    .setContentText("오늘 일정을 확인하세요!")
-                    .setContentInfo("INFO")
-                    .setContentIntent(pendingIntent);
-            if (notificationManager != null) {
-                // 노티피케이션 동작시킴
-                SimpleDateFormat fm = new SimpleDateFormat("yyyy.MM.dd");
-                Calendar calendar = Calendar.getInstance();
-                String CompareDate = fm.format(calendar.getTime());
-                String year = CompareDate.substring(0,10).substring(0,4);
-                String month = CompareDate.substring(0,10).substring(5,7);
-                String day = CompareDate.substring(8,10);
-                SharedPreferences sharedPreferences= getSharedPreferences("daily alarm", MODE_PRIVATE);
-                long time = sharedPreferences.getLong(year+"."+month+"."+day, calendar.getTimeInMillis());
-                if(calendar.getTimeInMillis()==time){
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                        startForeground(1, builder.build());
-                    }
-                    notificationManager.notify(1234, builder.build());
-                }
-            }
-        }
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
